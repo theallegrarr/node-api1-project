398612756a7761c2ea9a38d6f3ee4081a605043b
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-28 12:27:52 +0100

    app setup complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.get('*', handleDefault);
+
+
+function handleDefault(req, res){
+  res.json('no path here');
+}
+
+server.listen((process.env.PORT || 3400), () => {
+  console.log('listening on '+(process.env.PORT || 3400));
+});
