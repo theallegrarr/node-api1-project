ba9e992403790c789c2457baf5be15af62cb4ae1
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-28 12:42:32 +0100

    insert user feature added

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
+const db = require('./data/db');
 server.use(cors());
 server.use(express.json());
 
 server.get('*', handleDefault);
+server.post('/api/users', addUser);
 
 
 function handleDefault(req, res){
   res.json('no path here');
+}
+
+function addUser(req, res){
+  const newGuy={
+    name: req.body.name,
+    bio: req.body.bio,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at
+  }
+
+  db.insert(newGuy)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 server.listen((process.env.PORT || 3400), () => {
