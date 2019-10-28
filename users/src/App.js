cf81159096aa7efbfbd9d52ba5e24c53281dbd21
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-28 15:38:45 +0100

    style updated

--- users/src/App.js
+++ users/src/App.js
@@ -9,12 +9,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >.</a>
           <Users />
         
       </header>
